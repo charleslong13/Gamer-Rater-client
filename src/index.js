996cdb5d2d221aeb281dc789f8fe5369b9ffc37f
charleslong13 <charleslong13@icloud.com>
2022-02-07 16:33:31 -0600

    csl-initial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
+import './index.css';
+import {GamerRater} from "./components/GamerRater"
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+    <GamerRater />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
